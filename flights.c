c8f3b663d67d9b087be1297710476c2702b90d00
pshreyash11 <shreyashrpatil11@gmail.com>
2024-04-09 16:33:34 +0530

    mergerd floyd-warshall into flights.c file

--- flights.c
+++ flights.c
@@ -9,6 +9,7 @@ edge* createEdge(int dest, int w){
     temp->next = NULL;
     return temp;
 }
+
 graph* createGraph(int numberOfNodes){
     graph* g = (graph*) malloc(sizeof(graph));
     g->numberOfNodes = numberOfNodes;
@@ -21,7 +22,7 @@ graph* createGraph(int numberOfNodes){
 }
 void addEdge(graph* g, int src, int dest, int w){
     edge* temp = createEdge(dest, w);
-    int i = 0;
+
     // while(g->nodes[i]->id != src){
     //     i++;
     // }
@@ -66,6 +67,49 @@ void dijkstra(graph* g, int src){
         printf("%d => %d \n", i, dist[i]);
     }
 }
+
+void floydWarshall(graph* g) {
+    int V = g->numberOfNodes;
+    int dist[MAX_NODES][MAX_NODES];
+
+    // Initialize distance matrix
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            dist[i][j] = (i == j) ? 0 : INT_MAX;
+        }
+    }
+
+    // Update distance matrix
+    for (int u = 0; u < V; u++) {
+        for (edge* e = g->nodes[u]->edges; e != NULL; e = e->next) {
+            int v = e->dest;
+            int w = e->w;
+            dist[u][v] = w;
+        }
+    }
+
+    // Floyd-Warshall algorithm
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Print the shortest distances
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            printf("%d ", (dist[i][j] == INT_MAX) ? -1 : dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
 int main(){
     graph* g = createGraph(5);
     addEdge(g, 0, 1, 4);
@@ -75,6 +119,8 @@ int main(){
     addEdge(g, 2, 3, 3);
     addEdge(g, 2, 4, 6);
     addEdge(g, 3, 4, 7);
+    
     dijkstra(g, 0);
+    floydWarshall(g);
     return 0;
 }
